9d910fc103ffee19e6f2b5e19168bb3ec95b63dd
Khwaja Shaikh <109279460+Shkkhwaja@users.noreply.github.com>
2024-09-21 15:11:32 +0530

    add live url

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config()
 const app = express();
 
 // Middleware configuration
-app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL || "https://commentdemo.vercel.app/", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
